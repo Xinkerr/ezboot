968dd08839c2ccc2cd222b143fece30609b12c98
Xinkerr <634326056@qq.com>
2025-01-28 17:32:58 +0800

    修复擦除时少一个扇区

--- project/STM32F103_HAL/porting/ezb_flash/ezb_flash.c
+++ project/STM32F103_HAL/porting/ezb_flash/ezb_flash.c
@@ -21,6 +21,7 @@
 #include <ezb_flash.h>
 #include <string.h>
 #include <mcu_header.h>
+#include <mlog.h>
 
 #define WORD_ALIGNMENT          4
 
@@ -39,6 +40,8 @@ int ezb_flash_erase(uint32_t addr, uint32_t size) {
 
     // 计算页数
     numberOfPages = size / FLASH_PAGE_SIZE;
+    if(size % FLASH_PAGE_SIZE)
+        numberOfPages ++;
 
     // 配置擦除结构
     eraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
